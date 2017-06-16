b5d960bd532e5cb11a4f92fdbe054ce0016b4601
Brian Chang <brian.h.chang1@gmail.com>
2017-06-16 11:54:19 -0500

    re-work ajax calls - need an access token

--- models/Comment.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-var CommentSchema = new mongoose.Schema({
-  body: {type: String, default: ''},
-  username: {type: String, default: ''},
-  timestamp:{type: Date, default: Date.now}
-});
-
-module.exports = mongoose.model('CommentSchema', CommentSchema);
--- models/Zone.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var ZoneSchema = new mongoose.Schema({
-  name: {type: String, default: ''},
-  zipCodes: {type:Array, default:[]},
-  timestamp:{type: Date, default: Date.now}
-});
-
-module.exports = mongoose.model('ZoneSchema', ZoneSchema);
