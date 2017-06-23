83e0eb36064346b8a3e752ee5c80a05d51164649
gitbchang <brian.h.chang1@gmail.com>
2017-06-22 19:46:19 -0500

    initial redux setup

---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+export const SEARCH_ARTIST = 'search_artist';
+export const SEARCH_SONGS = 'search_songs';
--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
+import promise from 'redux-promise';
 
+import reducers from './reducers';
 import Home from './components/layout/Home';
+
+// redux dev tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// redux store
+const createStoreWithMiddleware = composeEnhancers(applyMiddleware(promise))(createStore);
 
 class App extends Component {
   render() {
     return (
       <div>
-        <BrowserRouter>
-          <Home />
-        </BrowserRouter>
+        <Provider store={createStoreWithMiddleware(reducers)}>
+          <BrowserRouter>
+            <Home />
+          </BrowserRouter>
+        </Provider>
       </div>
     );
   }
